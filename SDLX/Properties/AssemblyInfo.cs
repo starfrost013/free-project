43284f4b9df300ea30e06564a3b4f4cc25a86a30
Cosmo224 <mario64crashed@gmail.com>
2020-06-14 00:38:05 +0100

    Added SDLX library

---
+++ SDLX/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Emerald Simple DirectMedia Layer Rendering API (SDLX)")]
+[assembly: AssemblyDescription("Easy-to-use Simple DirectMedia Layer rendering services. ")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("avant-gardé eyes")]
+[assembly: AssemblyProduct("Emerald SDLX")]
+[assembly: AssemblyCopyright("Copyright © 2020 Cosmo/avant-gardé eyes. avant-gardé eyes is Copyright © 2018-2020 Connor Hyde.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2bece7a3-3bb0-4213-a097-2698058767b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
