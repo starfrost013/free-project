05096915f43a9a38c46c72b846860c3f58dd72da
Cosmo224 <mario64crashed@gmail.com>
2020-06-15 01:16:55 +0100

    9/14 => 9/15

--- free!/Init/EngineInit/Engine_BootNow.cs
+++ free!/Init/EngineInit/Engine_BootNow.cs
@@ -43,7 +43,7 @@ namespace Free
             GameTickTimer.Interval = 0.001; // We run AFAP as of 2020-05-26
 
             //TEMP:
-            Title = "free! (nightly build for June 14th, 2020)";
+            Title = "free! (nightly build for June 15th, 2020)";
 
             // Load everything that we can load at init
             LoadSettings();
