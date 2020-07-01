6d836a9b8ab67e1f2b31b711844630eb1a05c003
Cosmo224 <mario64crashed@gmail.com>
2020-07-01 22:53:42 +0100

    add sdl rect class

---
+++ SDLX/SDL_API_GetRect.cs
@@ -0,0 +1,24 @@
+﻿using Emerald.Utilities.Wpf2Sdl;
+using SDL2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDLX
+{
+    public partial class Game
+    {
+        public SDL.SDL_Rect SDL_GetRect(SDLPoint Position, SDLPoint Size)
+        {
+            return new SDL.SDL_Rect
+            {
+                x = (int)Position.X,
+                y = (int)Position.Y,
+                w = (int)Size.X,
+                h = (int)Size.Y
+            };
+        }
+    }
+}
