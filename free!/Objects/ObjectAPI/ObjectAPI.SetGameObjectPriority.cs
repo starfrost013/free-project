043bae5e56603b20a0d8ec3e0c06e62a4e028011
Cosmo224 <mario64crashed@gmail.com>
2020-07-05 01:16:36 +0100

    fix compile error

--- free!/Objects/ObjectAPI/ObjectAPI.SetGameObjectPriority.cs
+++ free!/Objects/ObjectAPI/ObjectAPI.SetGameObjectPriority.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Free.Objects.ObjectAPI
+namespace Free
 {
     // TEMP in freesdl.
     public partial class FreeSDL
