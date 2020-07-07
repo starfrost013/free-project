38ddf4d2b4d749d7913f73065544dc1d86632149
Cosmo224 <mario64crashed@gmail.com>
2020-07-07 01:17:46 +0100

    final

--- free!/Properties/AssemblyInfo.cs
+++ free!/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.21.1448.89")]
-[assembly: AssemblyFileVersion("2.21.1448.89")]
+[assembly: AssemblyVersion("2.21.1449.89")]
+[assembly: AssemblyFileVersion("2.21.1449.89")]
