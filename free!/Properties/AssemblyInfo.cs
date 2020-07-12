7a88b6aea1b59501f0745788ba341092bb77ef8a
Cosmo224 <mario64crashed@gmail.com>
2020-07-12 15:30:16 +0100

    forgot some files

--- free!/Properties/AssemblyInfo.cs
+++ free!/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.21.1457.98")]
-[assembly: AssemblyFileVersion("2.21.1457.98")]
+[assembly: AssemblyVersion("2.21.1457.99")]
+[assembly: AssemblyFileVersion("2.21.1457.99")]
