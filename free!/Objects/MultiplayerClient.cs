fd5acd56691bc927e58cebb6a98e4043e3d33f98
starfrost <mario64crashed@gmail.com>
2021-01-29 22:53:27 +0000

    player client information

---
+++ free!/Objects/MultiplayerClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Free
+{
+    public class MultiplayerClientInfo
+    {
+        /// <summary>
+        /// MP: Player name
+        /// </summary>
+        public string UName { get; set; }
+
+        /// <summary>
+        /// MP: User Guid
+        /// </summary>
+        public Guid UGuid { get; set; }
+
+        /// <summary>
+        /// MP: User ping
+        /// </summary>
+        public int UPing { get; set; }
+    }
+}
---
+++ free!/Objects/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Free
+{
+    public class Player : Humanoid
+    {
+
+        /// <summary>
+        /// Is this player contro
+        /// </summary>
+        public bool IsLocalPlayer { get; set; }
+        public MultiplayerClientInfo MPClientInfo { get; set; }
+
+        public Player()
+        {
+            MPClientInfo = new MultiplayerClientInfo(); 
+        }
+
+    }
+}
