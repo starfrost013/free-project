5c01b427f6287d3b197da39ad5376057831aa45c
starfrost <mario64crashed@gmail.com>
2021-01-29 15:55:06 +0000

    PhysicalObject improvements

--- free!/Objects/Root/PhysicalObject.cs
+++ free!/Objects/Root/PhysicalObject.cs
@@ -20,5 +20,23 @@ namespace Free
         /// </summary>
         public SDLPoint Size { get; set; }
         public new void SetName(string NewName) => base.SetName(NewName);
+
+        public void SetPosition(SDLPoint NewPosition)
+        {
+            // temp
+            if (NewPosition.X >= 0 && NewPosition.X <= 9999999 && NewPosition.Y >= 0 && NewPosition.Y <= 9999999)
+            {
+                Position = NewPosition;
+            }
+        }
+
+        public void SetPosition(double X, double Y)
+        {
+            // temp
+            if (X >= 0 && X <= 9999999 && Y >= 0 && Y <= 9999999)
+            {
+                Position = new SDLPoint(X, Y); 
+            }
+        }
     }
 }
